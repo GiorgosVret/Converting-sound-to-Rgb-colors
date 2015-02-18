2beeb07371ed718625c994df3613b9415b1a41a8
Constantinos Lolas <costasdroid@gmail.com>
2015-02-18 15:40:10 +0200

    Proper way
    Καλό είναι να έχεις ξεχωριστό αρχείο για κάθε κώδικα που γράφεις.
    Το αρχικό Readme αρχείο να είναι μόνο οι πληροφορίες αυτού που περιέχει το repository σου
    Αν χρειάζεσαι βοήθεια, θα δημιουργήσεις issue που να περιγράφει το πρόβλημά σου
    
    Οπότε κάνε merge το δικό μου pull και συνεχίζουμε μετά

---
+++ sound2RGB.ino
@@ -0,0 +1,42 @@
+int DO = 2 ; //digital 
+int DA = A0; //analog 
+int sensorvalue = 0; 
+int redPin = 9;
+int greenPin = 10; 
+int bluePin = 11;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  analogWrite(redPin, 255);
+  analogWrite(greenPin, 255);
+  analogWrite(bluePin, 255);
+}
+
+void loop() {
+
+MicValue = analogRead(MicPin);
+
+if (sensorvalue > 530) {
+  analogWrite(bluePin,0);
+  delay(15);
+}
+
+if (sensorvalue > 540) {
+  analogWrite(bluePin,255);
+  analogWrite(greenPin, 0);
+  delay(60);
+}
+
+if (sensorvalue > 550) {
+  analogWrite(greenPin,255);
+  analogWrite(redPin, 0);
+  delay(140);
+}
+
+  analogWrite(greenPin, 255);
+  analogWrite(redPin, 255); analogWrite(bluePin,255);
+
+}
